40c7afa80a93165879498097d357b123c97ef28f
Trevor Elkins <elkins.trevor@gmail.com>
2014-04-28 03:15:17 -0400

    added license and removed unused imports

--- AnimationTesting/src/main/java/com/trevore/animationtesting/TypeWriterView.java
+++ AnimationTesting/src/main/java/com/trevore/animationtesting/TypeWriterView.java
@@ -2,19 +2,28 @@ package com.trevore.animationtesting;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Handler;
-import android.text.Editable;
 import android.text.InputType;
-import android.text.Selection;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 
 /**
- * Created by Trevor on 4/27/2014.
+ * Copyright 2014 Trevor Elkins
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
  */
 public class TypeWriterView extends FrameLayout {
 
